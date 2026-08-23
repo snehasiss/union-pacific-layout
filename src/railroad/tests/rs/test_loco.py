bc5e101473faf061f3ac8500c95d274fc35bd9d6
Snehasis Sinha <snehasiss@users.noreply.github.com>
2026-08-23 21:58:30 +0530

    test: import LocoType from loco

--- src/railroad/tests/rs/test_loco.py
+++ src/railroad/tests/rs/test_loco.py
@@ -11,70 +11,35 @@ from railroad.domain.control import Control, ControlType
 from railroad.domain.identity import EntityType, Identity
 from railroad.domain.model import Model
 from railroad.domain.prototype import Prototype, Purpose
-from railroad.rs.loco import Loco
-from railroad.rs.loco_type import LocoType
+from railroad.rs.loco import Loco, LocoType
 
 
 def make_identity() -> Identity:
-    return Identity(
-        id="L001",
-        entity_type=EntityType.LOCO,
-        railroad="Union Pacific",
-        reporting_mark="UP",
-        road_number="4014",
-    )
+    return Identity(id="L001", entity_type=EntityType.LOCO, railroad="Union Pacific", reporting_mark="UP", road_number="4014")
 
 
 def make_prototype() -> Prototype:
-    return Prototype(
-        builder="ALCo",
-        model="4-8-8-4",
-        nickname="Big Boy",
-        purpose=Purpose.FREIGHT,
-    )
+    return Prototype(builder="ALCo", model="4-8-8-4", nickname="Big Boy", purpose=Purpose.FREIGHT)
 
 
 def make_model() -> Model:
-    return Model(
-        maker="Athearn",
-        product="Genesis Big Boy",
-    )
+    return Model(maker="Athearn", product="Genesis Big Boy")
 
 
 def make_control() -> Control:
-    return Control(
-        type=ControlType.DCC,
-        light=True,
-        sound=True,
-        smoke=False,
-        decoder="Paragon4",
-        address=4014,
-    )
+    return Control(type=ControlType.DCC, light=True, sound=True, smoke=False, decoder="Paragon4", address=4014)
 
 
 def make_asset() -> Asset:
-    return Asset(
-        status=AssetStatus.OWNED,
-        source="Model Train Stuff",
-        price=599.99,
-        acquired=date(2026, 1, 1),
-    )
+    return Asset(status=AssetStatus.OWNED, source="Model Train Stuff", price=599.99, acquired=date(2026, 1, 1))
 
 
 def make_loco() -> Loco:
-    return Loco(
-        identity=make_identity(),
-        loco_type=LocoType.STEAM,
-        prototype=make_prototype(),
-        model=make_model(),
-        control=make_control(),
-        asset=make_asset(),
-    )
+    return Loco(identity=make_identity(), loco_type=LocoType.STEAM, prototype=make_prototype(), model=make_model(), control=make_control(), asset=make_asset())
 
 
 def test_loco_creation():
     loco = make_loco()
-
     assert isinstance(loco, Loco)
     assert loco.loco_type == LocoType.STEAM
     assert loco.prototype.model == "4-8-8-4"
@@ -85,7 +50,6 @@ def test_loco_creation():
 
 def test_loco_identity_properties():
     loco = make_loco()
-
     assert loco.id == "L001"
     assert loco.entity_type == EntityType.LOCO
     assert loco.railroad == "Union Pacific"
@@ -95,7 +59,6 @@ def test_loco_identity_properties():
 
 def test_loco_prototype_properties():
     loco = make_loco()
-
     assert loco.prototype_model == "4-8-8-4"
     assert loco.nickname == "Big Boy"
 
@@ -103,104 +66,46 @@ def test_loco_prototype_properties():
 def test_loco_accepts_diesel():
     loco = make_loco()
     loco.loco_type = LocoType.DIESEL
-
     assert loco.loco_type == LocoType.DIESEL
 
 
 def test_loco_accepts_turbine():
     loco = make_loco()
     loco.loco_type = LocoType.TURBINE
-
     assert loco.loco_type == LocoType.TURBINE
 
 
 def test_loco_accepts_missing_nickname():
-    prototype = Prototype(
-        builder="EMD",
-        model="SD40-2",
-        nickname=None,
-        purpose=Purpose.FREIGHT,
-    )
-
-    loco = Loco(
-        identity=make_identity(),
-        loco_type=LocoType.DIESEL,
-        prototype=prototype,
-        model=make_model(),
-        control=make_control(),
-        asset=make_asset(),
-    )
-
+    prototype = Prototype(builder="EMD", model="SD40-2", nickname=None, purpose=Purpose.FREIGHT)
+    loco = Loco(identity=make_identity(), loco_type=LocoType.DIESEL, prototype=prototype, model=make_model(), control=make_control(), asset=make_asset())
     assert loco.nickname is None
 
 
 def test_loco_rejects_invalid_identity():
     with pytest.raises(TypeError):
-        Loco(
-            identity="L001",
-            loco_type=LocoType.STEAM,
-            prototype=make_prototype(),
-            model=make_model(),
-            control=make_control(),
-            asset=make_asset(),
-        )
+        Loco(identity="L001", loco_type=LocoType.STEAM, prototype=make_prototype(), model=make_model(), control=make_control(), asset=make_asset())
 
 
 def test_loco_rejects_invalid_loco_type():
     with pytest.raises(TypeError):
-        Loco(
-            identity=make_identity(),
-            loco_type="steam",
-            prototype=make_prototype(),
-            model=make_model(),
-            control=make_control(),
-            asset=make_asset(),
-        )
+        Loco(identity=make_identity(), loco_type="steam", prototype=make_prototype(), model=make_model(), control=make_control(), asset=make_asset())
 
 
 def test_loco_rejects_invalid_prototype():
     with pytest.raises(TypeError):
-        Loco(
-            identity=make_identity(),
-            loco_type=LocoType.STEAM,
-            prototype="prototype",
-            model=make_model(),
-            control=make_control(),
-            asset=make_asset(),
-        )
+        Loco(identity=make_identity(), loco_type=LocoType.STEAM, prototype="prototype", model=make_model(), control=make_control(), asset=make_asset())
 
 
 def test_loco_rejects_invalid_model():
     with pytest.raises(TypeError):
-        Loco(
-            identity=make_identity(),
-            loco_type=LocoType.STEAM,
-            prototype=make_prototype(),
-            model="model",
-            control=make_control(),
-            asset=make_asset(),
-        )
+        Loco(identity=make_identity(), loco_type=LocoType.STEAM, prototype=make_prototype(), model="model", control=make_control(), asset=make_asset())
 
 
 def test_loco_rejects_invalid_control():
     with pytest.raises(TypeError):
-        Loco(
-            identity=make_identity(),
-            loco_type=LocoType.STEAM,
-            prototype=make_prototype(),
-            model=make_model(),
-            control="control",
-            asset=make_asset(),
-        )
+        Loco(identity=make_identity(), loco_type=LocoType.STEAM, prototype=make_prototype(), model=make_model(), control="control", asset=make_asset())
 
 
 def test_loco_rejects_invalid_asset():
     with pytest.raises(TypeError):
-        Loco(
-            identity=make_identity(),
-            loco_type=LocoType.STEAM,
-            prototype=make_prototype(),
-            model=make_model(),
-            control=make_control(),
-            asset="asset",
-        )
+        Loco(identity=make_identity(), loco_type=LocoType.STEAM, prototype=make_prototype(), model=make_model(), control=make_control(), asset="asset")
